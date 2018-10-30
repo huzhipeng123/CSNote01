7df05bcfd20f22c7dcb6f264366ca3e61e13f19d
huzhpm <huzhpm@yonyou.com>
2018-10-30 11:24:53 +0800

    '测试'

--- src/Demo0/demo4.java
+++ src/Demo0/demo4.java
@@ -1,9 +1,21 @@
 package Demo0;
 
+import java.util.Scanner;
+
 /**
  * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
  * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
  */
 public class demo4 {
-
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{
+                {1,   4,  7, 11, 15},
+                {2,   5,  8, 12, 19},
+                {3,   6,  9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+        };
+        System.out.println("请输入你要查找的数字：");
+        Scanner sc = new Scanner(System.in);
+    }
 }
